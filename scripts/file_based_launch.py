d2ec04e34e3b8c17f7833db484bef8e29da862a8
Miriam <miriam.mueller99@gmail.com>
2024-10-07 12:29:23 +0200

    generalized the file_based_launch.py file

--- scripts/file_based_launch.py
+++ scripts/file_based_launch.py
@@ -2,27 +2,23 @@ from start_program import CRISPRScreenAnalysis
 
 
 class Args:
-    input_file = (r"D:\D\Ausbildung\Master\1st year\Internships\NKI\Report\Program test\Datasets to be analysed"
-                  r"\dataset\input_data\7234_all_Brunello_library_target_genes-req.txt")
-    essential_genes = (r"D:\D\Ausbildung\Master\1st year\Internships\NKI\Report\Program test\Program data"
-                       r"\list_essentials_roderick.csv")
-    non_essential_genes = (r"D:\D\Ausbildung\Master\1st year\Internships\NKI\Report\Program test\Program data"
-                           r"\non_essentials_roderick.csv")
-    library_file = (r"D:\D\Ausbildung\Master\1st year\Internships\NKI\Report\Program test\Program data"
-                    r"\broadgpp-brunello-library-contents.txt")
-    target_samples = "t2_2d_tr,t2_3d_tr,t1_3d,t1_3d2d,t2_3d2d_ut,t2_3d2d_tr,t2_2d_tr,t2_2d_ut,t2_3d_tr,t2_3d_ut"
-    reference_samples = "t2_2d_ut,t2_3d_ut,t1_2d,t1_3d,t2_3d_ut,t2_3d_tr,t1_2d,t1_2d,t1_3d,t1_3d"
-    unwanted_columns = "guide_mm1_mismatch1,mismatch1_,nohit_cols,guide_mm1_nohit"
-    unwanted_rows = ""
-    unwanted_row_substrings = ":mismatch"
+    input_file = r"file\path\to\the\crispr_screen_read_counts.txt"
+    essential_genes = r"file\path\to\the\essential_genes.csv"
+    non_essential_genes = r"file\path\to\the\non_essential_genes.csv"
+    library_file = r"file\path\to\the\library_file.txt"
+    target_samples = "treated_conditions_within_input_file_comma_separated"  # e.g. t1_2d_tr,t2_2d_tr
+    reference_samples = "baseline_conditions_within_input_file_comma_separated"  # e.g. t1_2d_ut,t2_2d_ut
+    unwanted_columns = "unwanted_columns_within_input_file_comma_separated"  # e.g. guide_mm1_mismatch1,mismatch1_
+    unwanted_rows = "unwanted_rows_within_input_file_comma_separated"
+    unwanted_row_substrings = "unwanted_row_substrings_within_input_file_comma_separated"
     threshold_reads = 0
-    x_axis = "normZ"
+    x_axis = "normZ"  # or "log2 fold-change"
     threshold_fdr = 0.25
     top = 15
-    distribution_condition1 = "t1_2d"
-    distribution_condition2 = "t0"
-    replicate_type = "biological"
-    working_dir = r"D:\D\Ausbildung\Master\1st year\Internships\NKI\Report\Program test"
+    distribution_condition1 = "positive_control_sample"
+    distribution_condition2 = "negative_control_sample"
+    replicate_type = "biological"  # or "technical"
+    working_dir = r"folder\path\to\the\crispr_screen_analysis"
 
 
 if __name__ == "__main__":
