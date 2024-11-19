4005e69e9cbe4f9fd54a045e1d90e1df22331fc3
Miriam <miriam.mueller99@gmail.com>
2024-11-19 15:03:50 +0100

    reformatted the run_drugz.py file

--- scripts/run_drugz.py
+++ scripts/run_drugz.py
@@ -10,29 +10,31 @@ import sys
 
 
 class Args:
-    infile = os.path.abspath(fnmatch.filter(os.listdir('.'), '*drugz-input.txt')[0])
-    drugz_output_file = "./drugz/drugz_"
-    gRNA_outfile = "./drugz/gRNA_"
-    target_samples = sys.argv[1]
-    reference_samples = sys.argv[2]
-    remove_genes = None
-    unpaired = True
-    replicates = True
-    pseudocount = 5
-    half_window_size = 500
+    def __init__(self):
+        self.infile = os.path.abspath(fnmatch.filter(os.listdir('.'), '*drugz-input.txt')[0])
+        self.drugz_output_file = "./drugz/drugz_"
+        self.gRNA_outfile = "./drugz/gRNA_"
+        self.target_samples = sys.argv[1]
+        self.reference_samples = sys.argv[2]
+        self.remove_genes = None
+        self.unpaired = True
+        self.replicates = True
+        self.pseudocount = 5
+        self.half_window_size = 500
 
 
-target_samples_list = Args.target_samples.split(',')
-reference_samples_list = Args.reference_samples.split(',')
-
-# Create a DrugZ output folder (if it does not exist yet)
-os.makedirs("./drugz", exist_ok=True)
-
-# Call the drugZ_analysis function for each pair of target_sample and reference_sample
-for target_sample, reference_sample in zip(target_samples_list, reference_samples_list):
+if __name__ == '__main__':
     args = Args()
-    args.target_sample = target_sample
-    args.reference_sample = reference_sample
-    args.drugz_output_file = args.drugz_output_file + target_sample + "-" + reference_sample + ".txt"
-    args.gRNA_outfile = args.gRNA_outfile + target_sample + "-" + reference_sample + ".txt"
-    drugZ.drugz_analysis(args)
+    target_samples_list = args.target_samples.split(',')
+    reference_samples_list = args.reference_samples.split(',')
+
+    # Create a DrugZ output folder (if it does not exist yet)
+    os.makedirs("./drugz", exist_ok=True)
+
+    # Call the drugZ_analysis function for each pair of target_sample and reference_sample
+    for target_sample, reference_sample in zip(target_samples_list, reference_samples_list):
+        args.target_sample = target_sample
+        args.reference_sample = reference_sample
+        args.drugz_output_file = args.drugz_output_file + target_sample + "-" + reference_sample + ".txt"
+        args.gRNA_outfile = args.gRNA_outfile + target_sample + "-" + reference_sample + ".txt"
+        drugZ.drugz_analysis(args)
